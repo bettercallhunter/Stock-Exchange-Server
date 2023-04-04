90c726699b0897497bac25b80ecd77390333c938
anzhooou <anzhooou@163.com>
2023-04-04 17:18:44 -0400

    init atomic

--- handler.py
+++ handler.py
@@ -35,36 +35,36 @@ def handleCancel(Responseroot, child, account_id):
     # modify account balance and position
     stmt = select(Account).where(Account.id == account_id)
     account = session.scalar(stmt)
+    with session.begin_nested():
+        # if it is a buy order
+        if int(amount) > 0:
+            account.balance += amount * limit
+        # if it is a sell order
+        else:
+            account.position[order.sym] -= amount
+            flag_modified(account, "position")
+        # delete from open
+        stmt = delete(Open).where(Open.id == id)
+        session.execute(stmt)
+        # add to cancel
+        ######### WHAT IS THE TIME OF CANCEL?##########
+        cancel = Cancel(id=id, sym=order.sym, amount=order.amount,
+                        limit=order.limit, account_id=account_id, time=datetime.now())
 
-    # if it is a buy order
-    if int(amount) > 0:
-        account.balance += amount * limit
-    # if it is a sell order
-    else:
-        account.position[order.sym] -= amount
-        flag_modified(account, "position")
-    # delete from open
-    stmt = delete(Open).where(Open.id == id)
-    session.execute(stmt)
-    # add to cancel
-    ######### WHAT IS THE TIME OF CANCEL?##########
-    cancel = Cancel(id=id, sym=order.sym, amount=order.amount,
-                    limit=order.limit, account_id=account_id, time=datetime.now())
+        # find order in executed
 
-    # find order in executed
+        stmt = select(Executed).where(Executed.transId == id)
+        executed_order = session.scalar(stmt)
+        if executed_order is None:
+            cancel_response_success(
+                Responseroot, id, order.time, order.amount, order.time, order.amount, order.limit, False)
 
-    stmt = select(Executed).where(Executed.transId == id)
-    executed_order = session.scalar(stmt)
-    if executed_order is None:
-        cancel_response_success(
-            Responseroot, id, order.time, order.amount, order.time, order.amount, order.limit, False)
+        else:
 
-    else:
-
-        cancel_response_success(
-            Responseroot, id, order.time, order.amount, executed_order.time, executed_order.amount, executed_order.limit, True)
-    session.add(cancel)
-    session.commit()
+            cancel_response_success(
+                Responseroot, id, order.time, order.amount, executed_order.time, executed_order.amount, executed_order.limit, True)
+        session.add(cancel)
+        session.commit()
 
 
 def handleQuery(Responseroot, root):
@@ -101,41 +101,42 @@ def handleOrder(Responseroot, child, account_id) -> None:
     # first, check if there is a match
     stmt = select(Account).where(Account.id == account_id)
     account = session.execute(stmt).fetchone()
-    if account is None:
-        msg = "account does not exist"
-        order_response(Responseroot, False, sym, amount, limit, msg)
-        return
-    # if it is a buy order
-    if int(amount) > 0:
-        newBalance = account[0].balance - int(amount) * int(limit)
-        if newBalance < 0:
-            msg = "insufficient funds"
+    with session.begin_nested():
+        if account is None:
+            msg = "account does not exist"
             order_response(Responseroot, False, sym, amount, limit, msg)
             return
+        # if it is a buy order
+        if int(amount) > 0:
+            newBalance = account[0].balance - int(amount) * int(limit)
+            if newBalance < 0:
+                msg = "insufficient funds"
+                order_response(Responseroot, False, sym, amount, limit, msg)
+                return
 
-        account[0].balance = newBalance
+            account[0].balance = newBalance
 
-    # if it is a sell order
-    elif int(amount) < 0:
-        # check if have enough shares
-        if account[0].position is None or sym not in account[0].position or int(account[0].position[sym]) < abs(int(amount)):
-            msg = "insufficient shares"
-            order_response(Responseroot, False, sym, amount, limit, msg)
-            return
-        newAmount = int(account[0].position[sym]) - abs(int(amount))
-        # update position
-        account[0].position[sym] = newAmount
-        flag_modified(account[0], "position")
-    # add order to database
-    Transaction_id = getMaxId()+1
+        # if it is a sell order
+        elif int(amount) < 0:
+            # check if have enough shares
+            if account[0].position is None or sym not in account[0].position or int(account[0].position[sym]) < abs(int(amount)):
+                msg = "insufficient shares"
+                order_response(Responseroot, False, sym, amount, limit, msg)
+                return
+            newAmount = int(account[0].position[sym]) - abs(int(amount))
+            # update position
+            account[0].position[sym] = newAmount
+            flag_modified(account[0], "position")
+        # add order to database
+        Transaction_id = getMaxId()+1
 
-    new_order = Open(account_id=account_id, id=Transaction_id,
-                     sym=sym, amount=amount, limit=limit, time=datetime.now())
-    order_response(Responseroot, True, sym, amount,
-                   limit, "ok", Transaction_id)
+        new_order = Open(account_id=account_id, id=Transaction_id,
+                        sym=sym, amount=amount, limit=limit, time=datetime.now())
+        order_response(Responseroot, True, sym, amount,
+                    limit, "ok", Transaction_id)
 
-    session.add(new_order)
-    session.commit()
+        session.add(new_order)
+        session.commit()
     print("order placed")
     match_order(sym)
 
